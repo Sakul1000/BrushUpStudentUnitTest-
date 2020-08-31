25248bc00c8c82551b8c7b0686d789dc3a019fc0
Lukas Rosengaard <sakul10000@outlook.dk>
2020-08-31 09:26:32 +0200

    Add project files.

---
+++ BrushUpStudent/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrushUpStudent
+{
+    public enum Gender
+    {
+        Male,Female
+    }
+
+    public class Student
+    {
+        private string _name;
+        private string _address;
+        private int _semester;
+
+        //to make a test right clik the class and press "create unit tests" 
+        public Student(string name, string address, int semester, Gender gender)
+
+        {
+            Name = name;
+            address = address;
+            Gender = gender;
+            Semester = semester;
+
+        }
+
+        public string Name
+        {
+            get {return _name; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "name is null");
+                if (value.Length < 2) throw new ArgumentOutOfRangeException("name length must be at least 2 characters" );
+                _name = value;
+            }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("address", "Address is null");
+                _address = value;
+            }
+        }
+
+        public int Semester
+        {
+            get { return _semester; }
+            set
+            {
+                if (1 <= value && value <= 8) _semester = value;
+                else throw new ArgumentOutOfRangeException("semester", value, "Illegal semester");
+            }
+        }
+
+        public Gender Gender { get; set; }
+    }
+}
+
---
+++ BrushUpStudentTests/StudentTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrushUpStudent;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrushUpStudent.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {
+        private Student lu = new  Student("ib", "vanløse", 1, Gender.Male);
+
+        [TestMethod()]
+        public void StudentTest()
+        {
+            Assert.AreEqual("ib", lu.Name);
+            
+        }
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => lu.Name = "x");
+        }
+    }
+}
